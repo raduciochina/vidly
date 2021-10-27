4da1711ea8d53c8e25fe1162ef7b0abc2bb389a6
Radu Ciochina <ciochina7@gmail.com>
2021-10-27 20:29:42 +0300

    Second Commit

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,25 +11,46 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+   
+            return View(movies);
+                    
+        }
+
+
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id=1, Name = "Shrek"},
+                new Movie{Id=2, Name = "Brooklyn Nine-Nine"}
+            };
+        }
+
         public ActionResult Random()
         {
-            var movie = new Movie { Name = "Shrek" };
+
+            var movie = new Movie() { Name = "Shrek 2" };
             var customers = new List<Customer>
+        {
+            new Customer {Name = "MIRCEA"},
+            new Customer {Name = "MARIAN"}
+        };
+
+
+
+            var viewModel = new RandomMovieViewModel
             {
-                new Customer {Name = "MIRCEA"},
-                new Customer {Name = "MARIAN"}
-            };
-
-            // ViewData["Movie"] = movie; nerecomandat;
-
-            var viewModel = new RandomMovieViewModel {
                 Movie = movie,
                 Customers = customers
             };
-   
-           return View(viewModel);
-                    
+
+            return View(viewModel);
         }
+
         //public ActionResult Edit(int id)
         //{
         //    return Content("id=" + id);
@@ -44,7 +65,7 @@ namespace Vidly.Controllers
         //    if (String.IsNullOrWhiteSpace(sortBy))
         //    {
         //        sortBy = "Name";
-           
+
         //    }
         //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         //}
