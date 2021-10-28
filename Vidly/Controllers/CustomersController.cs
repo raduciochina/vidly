80462dc875bc4c208e4eaacb95c1c98e1cd53b14
Radu Ciochina <ciochina7@gmail.com>
2021-10-28 13:06:16 +0300

    Add database

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -4,22 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
-
+using System.Data.Entity;
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
-
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
         
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -27,15 +36,5 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer{Id = 1, Name = "Marian Popescu"},
-                new Customer{Id = 2, Name = "George Ionescu"},
-                new Customer{Id = 3, Name = "Mirel Radoi"}
-
-            };
-        }
     }
 }
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,31 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies/Random
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
    
             return View(movies);
                     
         }
 
-
-
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if(movie == null)
             {
-                new Movie{Id=1, Name = "Shrek"},
-                new Movie{Id=2, Name = "Brooklyn Nine-Nine"}
-            };
+                return HttpNotFound();
+            }
+
+            return View(movie);
         }
+
+
+
 
         public ActionResult Random()
         {
